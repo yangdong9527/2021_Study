a5bf9d0b04458ee887e63e75f0379f2a4cb7cd20
东 <18855238097@163.com>
2021-05-11 18:31:09 +0800

    first commit

--- TypeScript/code/01/interface.ts
+++ TypeScript/code/01/interface.ts
@@ -9,39 +9,43 @@
  * 3. 类应用一个接口, 将接口的属性定义到类里面
  */
 
-interface Person {
-  name: string;
-  readonly age: number; // 只读
-  sex?: string; // 该属性可有可无
-  [propsName: string]: any; // 解决字面量的方式使用 多余的参数报错
-  say(): string;
-}
+// interface Person {
+//   name: string;
+//   readonly age: number; // 只读
+//   sex?: string; // 该属性可有可无
+//   [propsName: string]: any; // 解决字面量的方式使用 多余的参数报错
+//   say(): string;
+// }
 
-const getPersonName = (person: Person): void => {
-  console.log(person.name);
-};
+// const getPersonName = (person: Person): void => {
+//   console.log(person.name);
+// };
 
-//  字面量的方式使用和 赋值的方式使用
-// 如果没有加上面的 [propsName: string] 而使用这种字面量的方式 就会报错
-getPersonName({
-  name: "yd",
-  age: 12,
-  say: () => {
-    return "123";
-  },
-  color: "black",
-});
+// //  字面量的方式使用和 赋值的方式使用
+// // 如果没有加上面的 [propsName: string] 而使用这种字面量的方式 就会报错
+// getPersonName({
+//   name: "yd",
+//   age: 12,
+//   say: () => {
+//     return "123";
+//   },
+//   color: "black",
+// });
 
-// 接口的继承
-interface Teachers extends Person {
-  teach(): string;
-}
+// // 接口的继承
+// interface Teachers extends Person {
+//   teach(): string;
+// }
 
-// 类应用接口
-class Abc implements Person {
-  name: "yd";
-  age: 12;
-  say() {
-    return "123";
-  }
-}
+// // 类应用接口
+// class Abc implements Person {
+//   name: "yd";
+//   age: 12;
+//   say() {
+//     return "123";
+//   }
+// }
+
+let a: number[] = [1,2,3]
+let b: number[] = a
+b[0] = 1
---
+++ Vue3.0 仿知乎/zheye/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <column-list :list="list" ></column-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { columnProp } from './components/ColumnList.vue'
+
+const testData: columnProp[] = [
+  {
+    id: 1,
+    avatar: 'https://vue3js.cn/docs/logo.png',
+    description: 'asdf',
+    title: 'asdf'
+  },
+  {
+    id: 2,
+    avatar: 'https://vue3js.cn/docs/logo.png',
+    description: 'asdf',
+    title: 'asdf'
+  },
+  {
+    id: 3,
+    avatar: 'https://vue3js.cn/docs/logo.png',
+    description: 'asdf',
+    title: 'asdf'
+  }
+]
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
